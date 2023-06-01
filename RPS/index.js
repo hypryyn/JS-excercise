1ed271da2f270ea994b3983bfa7296c4cd5d28c8
Yayan <111275294+hypryyn@users.noreply.github.com>
2023-06-01 20:04:21 +0700

    I know I lack on some basic programming fundmental

---
+++ RPS/index.js
@@ -0,0 +1,17 @@
+// let randomHand = Math.floor(Math.random() * 3);
+let handSign = [];
+
+if (randomHand = 0) {
+    handSign = "Rock"
+} else if (randomHand = 1) {
+    handSign = "Paper"
+} else {
+    handSign = "Scisor"
+}
+
+for (let i = 0; i <= 10; i++) {
+    let randomHand = Math.floor(Math.random() * 3);
+    console.log(randomHand);
+}
+
+// console.log(randomHand);
