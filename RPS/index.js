726af541301aaa1d7581e46e9d1c2b7ac5ea9a2b
Yayan <111275294+hypryyn@users.noreply.github.com>
2023-06-02 21:19:37 +0700

    trying different approach to iterate different array

--- RPS/index.js
+++ RPS/index.js
@@ -1,17 +1,23 @@
 // let randomHand = Math.floor(Math.random() * 3);
-let handSign = [];
-
-if (randomHand = 0) {
-    handSign = "Rock"
-} else if (randomHand = 1) {
-    handSign = "Paper"
-} else {
-    handSign = "Scisor"
-}
-
-for (let i = 0; i <= 10; i++) {
-    let randomHand = Math.floor(Math.random() * 3);
-    console.log(randomHand);
-}
-
 // console.log(randomHand);
+
+let sign = () => {
+    let handOutput = '';
+    let handSign = [];
+    for (let i = 0; i < 10; i++) {
+        let randomHand = Math.floor(Math.random() * 3);
+        if (randomHand = 0) {
+            handOutput = "Rock"
+        } else if (randomHand = 1) {
+            handOutput = "Paper"
+        } else {
+            handOutput = "Scisor"
+        }
+        handSign.push(handOutput);
+        return console.log(handOutput);
+        // return console.log(Math.random());
+    };
+
+}
+
+sign();
