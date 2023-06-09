e6efe21fab3740bab7e25593b284e5a37f9cb596
Yayan <111275294+hypryyn@users.noreply.github.com>
2023-06-09 21:12:45 +0700

    revisit pattern

--- Array/pattern.js
+++ Array/pattern.js
@@ -1,10 +1,21 @@
 function printPattern(range) {
-    for(var i=1;i<=range;i++){
-      var str="";
-      for(var j=1;j<=i;j++){
-        str += j+" ";
-      }
-      console.log(str);
+  for (var i = 1; i <= range; i++) {
+    var str = "";
+    for (var j = 1; j <= i; j++) {
+      str += j + " ";
     }
+    console.log(str);
   }
-  printPatter(8);
+}
+printPattern(8);
+
+function triangle(depth) {
+  for (let i = 1; i < depth; i++) {
+    var strg = "";
+    for (let j = 5; j <= i; j++) {
+      strg += j + " ";
+    }
+    console.log(strg);
+  }
+}
+triangle(9);
