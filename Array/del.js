02e03ab4137b8e918c887854fa288a2f1619d9eb
Yayan <111275294+hypryyn@users.noreply.github.com>
2023-06-09 21:26:57 +0700

    revisit del

--- Array/del.js
+++ Array/del.js
@@ -10,3 +10,16 @@ function deleteElement(arr, ele) {
   arr = deleteElement(arr, 56)
   
   console.log(arr); //[23, 4, 78, 5, 63, 45, 210]
+
+  function delEll(arry, ele) {
+    for (let i=0;i<arry.length;i++){
+      if(arry[i]==ele){
+        arry.splice(i,1);
+      }
+    }
+    return arry;
+  }
+  let arry = [5,3,4,5,7,2,8,5,6,4];
+  arry = delEll(arry, 5);
+
+  console.log(arry);
