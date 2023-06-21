f86663de2397bb7c9e1989821b868a2f6cde0418
Yayan <111275294+hypryyn@users.noreply.github.com>
2023-06-21 19:13:57 +0700

    revisit largest

--- Array/largest.js
+++ Array/largest.js
@@ -1,6 +1,16 @@
-var arr = [2, 45, 3, 67, 34, 567, 34, 345, 123];  
-var largest = arr[0];
-for(var i=0;i<arr.length;i++){
-   largest = arr[i]>largest ? arr[i]:largest; //Check largest number
+// var arr = [2, 45, 3, 67, 34, 567, 34, 345, 123];  
+// var largest = arr[0];
+// for (var i = 0; i < arr.length; i++) {
+//    largest = arr[i] > largest ? arr[i] : largest; //Check largest number
+// }
+// console.log(largest) //567
+
+////
+
+let arr = [4, 5, 90, 23, 32, 11, 70];
+
+let biggest = arr[0];
+for (let j = 0; j < arr.length; j++) {
+   biggest = arr[j] > biggest ? arr[j] : biggest;
 }
-console.log(largest) //567
+console.log(biggest);
