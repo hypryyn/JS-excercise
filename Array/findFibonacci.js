8c3c9030b7ddfd0cdf5b7ec3b86c9ca0ae22c73b
Yayan <111275294+hypryyn@users.noreply.github.com>
2023-06-24 12:22:13 +0700

    arrow function revisit

--- Array/findFibonacci.js
+++ Array/findFibonacci.js
@@ -1,15 +1,40 @@
-function findFibonacci(num) {
+// function findFibonacci(num) {
+//     var n1=0;
+//     var n2=1;
+//     var count=2;
+//     var n3;
+//     while(true){
+//       n3=n1+n2;
+//       if(n3==num){
+//          console.log(`Element ${num} is present in index ${count}`)
+//          break;
+//        }     
+//        else if(n3>num){
+//          console.log("Element not present");
+//          break;
+//        }
+//        n1=n2;
+//        n2=n3;
+//        count++;
+//     }
+//   }
+  
+//   findFibonacci(13) //"Element 13 is present in index 7"
+//   findFibonacci(144) //"Element 144 is present in index 12"
+
+
+  fFibo = (numb) => {
     var n1=0;
     var n2=1;
     var count=2;
     var n3;
     while(true){
       n3=n1+n2;
-      if(n3==num){
-         console.log(`Element ${num} is present in index ${count}`)
+      if(n3==numb){
+         console.log(`Element ${numb} is present in index ${count}`)
          break;
        }     
-       else if(n3>num){
+       else if(n3>numb){
          console.log("Element not present");
          break;
        }
@@ -18,6 +43,6 @@ function findFibonacci(num) {
        count++;
     }
   }
-  
-  findFibonacci(13) //"Element 13 is present in index 7"
-  findFibonacci(144) //"Element 144 is present in index 12"
+
+  fFibo(13)
+  fFibo(144)
