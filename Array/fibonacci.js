d3b390e41ca897d00f5d94508e022f2965184f7f
Yayan <111275294+hypryyn@users.noreply.github.com>
2023-06-25 21:11:48 +0700

    revisit fibonacci

--- Array/fibonacci.js
+++ Array/fibonacci.js
@@ -1,16 +1,33 @@
-function fibonacci(n) {
-    var n1=0;
-    var n2=1;
-    var count=2;    
-    var n3;
-    console.log(n1,n2);
-    while(count<n){
-     n3=n1+n2;
-     console.log(n3); //print current element
-     n1=n2;
-     n2=n3;
-     count++;
-    }
+// function fibonacci(n) {
+//     var n1=0;
+//     var n2=1;
+//     var count=2;    
+//     var n3;
+//     console.log(n1,n2);
+//     while(count<n){
+//      n3=n1+n2;
+//      console.log(n3); //print current element
+//      n1=n2;
+//      n2=n3;
+//      count++;
+//     }
+//   }
+
+//   fibonacci(8) //0 1 1 2 3 5 8 13
+
+fibonacci = (n) => {
+  let n1 = 0;
+  let n2 = 1;
+  let count = 2;
+  let n3;
+  console.log(n1, n2);
+  while (count < n) {
+    n3 = n1 + n2;
+    console.log(n3); //print current element
+    n1 = n2;
+    n2 = n3;
+    count++;
   }
-  
-  fibonacci(8) //0 1 1 2 3 5 8 13
+}
+
+fibonacci(8);
