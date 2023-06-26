533af9db0ebb77fd6739ba2393fdc4488b916206
Yayan <111275294+hypryyn@users.noreply.github.com>
2023-06-26 22:02:51 +0700

    revisit count

--- Array/count.js
+++ Array/count.js
@@ -1,10 +1,21 @@
-function digitCount(num) {
-    var count=0; //return 1 for pow=0
-    while(num!=0){
-      num=Math.floor(num/10);
-      count++;
-    }
-    return count;
+// function digitCount(num) {
+//     var count=0; //return 1 for pow=0
+//     while(num!=0){
+//       num=Math.floor(num/10);
+//       count++;
+//     }
+//     return count;
+//   }
+
+
+nCount = (num) => {
+  let count=0;
+  while(num!=0){
+    num=Math.floor(num/10);
+    count++;
   }
-  console.log(digitCount(8367)); //4
-  console.log(digitCount(563349)); //6
+  return count;
+}
+
+console.log(nCount(8367)); //4
+console.log(nCount(563349)); //6
