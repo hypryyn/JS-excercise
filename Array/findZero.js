ec5fbe26d4c0ea39af2b92beb71b61f306db5a3a
Yayan <111275294+hypryyn@users.noreply.github.com>
2023-06-27 12:22:05 +0700

    revisit zero count

--- Array/findZero.js
+++ Array/findZero.js
@@ -1,9 +1,18 @@
-var arr = [[0,1,1],[0,1,0],[1,0,0]];
+var arr = [[0, 1, 1], [0, 1, 0], [1, 0, 0]];
 var zeroCount = 0;
-for(var i=0;i<arr.length;i++){
-  for(var j=0;j<arr[0].length;j++){
-    zeroCount = arr[i][j]==0 ? zeroCount+1 : zeroCount; 
+for (var i = 0; i < arr.length; i++) {
+  for (var j = 0; j < arr[0].length; j++) {
+    zeroCount = arr[i][j] == 0 ? zeroCount + 1 : zeroCount;
   }
 }
 
 console.log(zeroCount) //5
+
+let zero = 0;
+for (let i = 0; i < arr.length; i++) {
+  for (let j = 0; j < arr[0].length; j++) {
+    zero = arr[i][j] == 0 ? zero + 1 : zero;
+  }
+}
+
+console.log(zero);
