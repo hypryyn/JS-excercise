33cfbe6e31e0ebbc1ef06ab6255e10376c7f2e85
Yayan <111275294+hypryyn@users.noreply.github.com>
2023-06-27 12:16:03 +0700

    revisit sum

--- Array/sum.js
+++ Array/sum.js
@@ -1,10 +1,21 @@
 function digitSum(num) {
-    var sum=0;
-    while(num!=0){
-     sum += num % 10;
-     num = Math.floor(num/10);
-    }
-    return sum;
+  var sum = 0;
+  while (num != 0) {
+    sum += num % 10;
+    num = Math.floor(num / 10);
   }
-  console.log(digitSum(4367)); //20
-  console.log(digitSum(56349)); //27
+  return sum;
+}
+console.log(digitSum(4367)); //20
+console.log(digitSum(56349)); //27
+
+nSum = (numb) => {
+  let sum = 0;
+  while (numb != 0) {
+    sum += numb % 10;
+    numb = Math.floor(numb / 10);
+  }
+  return sum;
+}
+
+console.log(nSum(57849));
