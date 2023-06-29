74a59528cb00379d29945f4466359f681fd25c73
Yayan <111275294+hypryyn@users.noreply.github.com>
2023-06-29 23:24:40 +0700

    revisit letter count

--- Array/countLetter.js
+++ Array/countLetter.js
@@ -14,3 +14,21 @@ for (var i = 0; i < 26; i++) {
     console.log(`Character ${i + 'a'} has occured ${count[i]} number of times`);
   }
 }
+
+let letter = "Matter";
+let nWord = new Array(26);
+str = letter.toLowerCase();
+for (let i = 0; i < letter.length; i++) {
+  if (nWord[letter.charCodeAt(i) - 97]) {
+    nWord[letter.charCodeAt(i) - 97]++; // Increment count
+  }
+  else {
+    nWord[letter.charCodeAt(i) - 97] = 1; // Add entry
+  }
+}
+for (let i = 0; i < 26; i++) {
+  if (nWord[i]) {
+    console.log(`Character ${i + 'a'} has occured ${nWord[i]} number of times`);
+  }
+}
+
