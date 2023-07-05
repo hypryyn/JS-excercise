035339617e4604d85268f883c05caa5f5683571f
Yayan <111275294+hypryyn@users.noreply.github.com>
2023-07-05 12:27:36 +0700

    find Least Common Multiple

---
+++ Array/LCM.js
@@ -0,0 +1,12 @@
+const num1 = 4;
+const num2 = 6;
+
+let min = (num1 > num2) ? num1 : num2;
+
+while (true) {
+    if (min % num1 == 0 && min % num2 == 0) {
+        console.log(`The LCM of ${num1} and ${num2} is ${min}`);
+        break;
+    }
+    min++;
+}
