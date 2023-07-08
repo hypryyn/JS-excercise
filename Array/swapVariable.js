df3d69083e004b5310996a0e327f7274e190d723
Yayan <111275294+hypryyn@users.noreply.github.com>
2023-07-08 12:40:11 +0700

    swapping variable

---
+++ Array/swapVariable.js
@@ -0,0 +1,16 @@
+//JavaScript program to swap two variables
+
+//take input from the users
+let a = 'super';
+let b = 'great';
+
+//create a temporary variable
+let temp;
+
+//swap variables
+temp = a;
+a = b;
+b = temp;
+
+console.log(`The value of a after swapping: ${a}`);
+console.log(`The value of b after swapping: ${b}`);
