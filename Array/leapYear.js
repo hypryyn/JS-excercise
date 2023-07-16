0e7a5bbc595a4b3772267a47aed4506f2f817009
Yayan <111275294+hypryyn@users.noreply.github.com>
2023-07-16 07:42:50 +0700

    another method

--- Array/leapYear.js
+++ Array/leapYear.js
@@ -10,3 +10,17 @@ function checkLeapYear(year) {
 const year = 2024;
 
 checkLeapYear(year);
+
+function checkLeapYear(year1) {
+
+    const leap = new Date(year1, 1, 29).getDate() === 29;
+    if (leap) {
+        console.log(year1 + ' is a leap year');
+    } else {
+        console.log(year1 + ' is not a leap year');
+    }
+}
+
+const year1 = 2002;
+
+checkLeapYear(year1);
