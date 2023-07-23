8f486502766aaa25a1d302237bc065b8208f432e
Yayan <111275294+hypryyn@users.noreply.github.com>
2023-07-23 19:41:56 +0700

    v2

--- Array/splitString.js
+++ Array/splitString.js
@@ -3,3 +3,13 @@ const string = 'Learning JavaScript Program';
 const result = string.replace(/a/g, "A");
 
 console.log(result);
+
+// program to replace all instances of character in a string
+
+const string2 = 'Learning JavaScript Program';
+
+const splitString = string.split('a');
+
+const result2 = splitString.join('A');
+
+console.log(result2);
