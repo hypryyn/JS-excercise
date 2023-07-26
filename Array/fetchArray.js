f12a152909761c24286fdd1af5078d195394282b
Yayan <111275294+hypryyn@users.noreply.github.com>
2023-07-26 21:03:37 +0700

    fetch object

---
+++ Array/fetchArray.js
@@ -0,0 +1,14 @@
+function extractValue(arr, prop) {
+
+    // extract value from property
+    let extractedValue = arr.map(item => item[prop]);
+
+    return extractedValue;
+
+}
+
+const objArray = [{a: 1, b: 2}, {a: 4, b: 5}, {a: 8, b: 9}];
+
+// passing an array of objects and property 'a' to extract
+const result = extractValue(objArray, 'a');
+console.log(result);
