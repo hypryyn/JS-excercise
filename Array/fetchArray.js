1f0787898df03e38fc2da1252893282b56d7f929
Yayan <111275294+hypryyn@users.noreply.github.com>
2023-07-27 23:12:25 +0700

    v2 using loops

--- Array/fetchArray.js
+++ Array/fetchArray.js
@@ -1,10 +1,30 @@
+// function extractValue(arr, prop) {
+
+//     // extract value from property
+//     let extractedValue = arr.map(item => item[prop]);
+
+//     return extractedValue;
+
+// }
+
+// const objArray = [{a: 1, b: 2}, {a: 4, b: 5}, {a: 8, b: 9}];
+
+// // passing an array of objects and property 'a' to extract
+// const result = extractValue(objArray, 'a');
+// console.log(result);
+
+//using loops
+
 function extractValue(arr, prop) {
 
-    // extract value from property
-    let extractedValue = arr.map(item => item[prop]);
+    let extractedValue = [];
 
+    for (let i=0; i < arr.length ; ++i) {
+
+        // extract value from property
+        extractedValue.push(arr[i][prop]);
+    }
     return extractedValue;
-
 }
 
 const objArray = [{a: 1, b: 2}, {a: 4, b: 5}, {a: 8, b: 9}];
