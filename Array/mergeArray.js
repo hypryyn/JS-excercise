6af8f2f248c01ee1f950bdd91bb36e8f02923d3a
Yayan <111275294+hypryyn@users.noreply.github.com>
2023-07-29 12:31:25 +0700

    v2

--- Array/mergeArray.js
+++ Array/mergeArray.js
@@ -19,3 +19,22 @@ const array2 = [2, 3, 5]
 // calling the function
 // passing array argument
 getUniqueAfterMerge(array1, array2);
+
+// Using Spread Syntax and Set
+
+function getUniqueAfterMerge1(arr3, arr4){
+
+    // merge two arrays
+    let arr = [...arr1, ...arr2];
+
+    // removing duplicate
+    let uniqueArr = [...new Set(arr)];
+
+    console.log(uniqueArr);
+}
+
+const array3 = [1, 2, 3];
+const array4 = [2, 3, 5]
+
+// calling the function
+getUniqueAfterMerge1(array1, array2);
