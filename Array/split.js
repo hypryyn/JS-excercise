63b0ae0838957783f90223622042abc44c8d89fb
Yayan <111275294+hypryyn@users.noreply.github.com>
2023-07-31 22:27:26 +0700

    take v2

--- Array/split.js
+++ Array/split.js
@@ -12,3 +12,19 @@ function splitIntoChunk(arr, chunk) {
 const array = [1, 2, 3, 4, 5, 6, 7, 8];
 const chunk = 2;
 splitIntoChunk(array, chunk);
+
+// or
+
+function splitArrIntoChunk(Arr1, chunk1) {
+
+    while(arr.length > 0) {
+
+        let tempArray;
+        tempArray = Arr1.splice(0, chunk1);
+        console.log(tempArray);
+    }
+}
+
+const array1 = [1, 2, 3, 4, 5, 6, 7, 8];
+const chunk1 = 2;
+splitIntoChunk(array, chunk);
