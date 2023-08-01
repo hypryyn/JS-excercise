cf625c4b62731b5ea00c70c7ba71e0206d996891
Yayan <111275294+hypryyn@users.noreply.github.com>
2023-08-01 23:38:15 +0700

    empty an array

--- Array/split.js
+++ Array/split.js
@@ -1,30 +1,46 @@
-function splitIntoChunk(arr, chunk) {
+// function splitIntoChunk(arr, chunk) {
 
-    for (i=0; i < arr.length; i += chunk) {
+//     for (i=0; i < arr.length; i += chunk) {
 
-        let tempArray;
-        tempArray = arr.slice(i, i + chunk);
-        console.log(tempArray);
-    }
+//         let tempArray;
+//         tempArray = arr.slice(i, i + chunk);
+//         console.log(tempArray);
+//     }
 
+// }
+
+// const array = [1, 2, 3, 4, 5, 6, 7, 8];
+// const chunk = 2;
+// splitIntoChunk(array, chunk);
+
+// // or
+
+// function splitArrIntoChunk(Arr1, chunk1) {
+
+//     while(arr.length > 0) {
+
+//         let tempArray;
+//         tempArray = Arr1.splice(0, chunk1);
+//         console.log(tempArray);
+//     }
+// }
+
+// const array1 = [1, 2, 3, 4, 5, 6, 7, 8];
+// const chunk1 = 2;
+// splitIntoChunk(array, chunk);
+
+// empty an array
+
+function emptyArray(arr) {
+
+    // substituting new array
+    arr = [];
+    
+    return arr;
 }
 
-const array = [1, 2, 3, 4, 5, 6, 7, 8];
-const chunk = 2;
-splitIntoChunk(array, chunk);
+const array = [1, 2 ,3];
+console.log(array);
 
-// or
-
-function splitArrIntoChunk(Arr1, chunk1) {
-
-    while(arr.length > 0) {
-
-        let tempArray;
-        tempArray = Arr1.splice(0, chunk1);
-        console.log(tempArray);
-    }
-}
-
-const array1 = [1, 2, 3, 4, 5, 6, 7, 8];
-const chunk1 = 2;
-splitIntoChunk(array, chunk);
+const result = emptyArray(array);
+console.log(result);
