2282d99124a046fb7325e33f7bc8fc00cccbc023
Yayan <111275294+hypryyn@users.noreply.github.com>
2023-08-11 23:37:30 +0700

    revision upload

---
+++ Array/mergeObject.js
@@ -0,0 +1,15 @@
+// object 1
+const person = {
+    name: 'Jack',
+    age:26
+}
+
+// object 2
+const student = {
+    gender: 'male'
+}
+
+// merge two objects
+const newObj = Object.assign(person, student);
+
+console.log(newObj);
