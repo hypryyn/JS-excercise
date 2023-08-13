2e328f7bffe3f4f4cdf12a0f1ef50071d0e7dba2
Yayan <111275294+hypryyn@users.noreply.github.com>
2023-08-13 21:28:13 +0700

    count the number of value in an object

---
+++ Array/countObject.js
@@ -0,0 +1,16 @@
+const student = { 
+    name: 'John',
+    age: 20,
+    hobbies: ['reading', 'games', 'coding'],
+};
+
+let count = 0;
+
+// loop through each key/value
+for(let key in student) {
+
+    // increase the count
+    ++count;
+}
+
+console.log(count);
