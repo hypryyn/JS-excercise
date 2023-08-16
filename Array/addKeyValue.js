97354e82c4046b65f2bdb910fa771e35db2412f7
Yayan <111275294+hypryyn@users.noreply.github.com>
2023-08-16 23:16:20 +0700

    v2 for adding key value pair

--- Array/addKeyValue.js
+++ Array/addKeyValue.js
@@ -9,3 +9,15 @@ person.height = 5.4;
 
 console.log(person);
 
+// using square bracket
+
+const person2 = {
+    name: 'Monica',
+    age: 22,
+    gender: 'female'
+}
+
+// add a key/value pair
+person2['height'] = 5.4;
+
+console.log(person2);
