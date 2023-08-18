540b519fec9380412bfe863891565f2b84c0d47f
Yayan <111275294+hypryyn@users.noreply.github.com>
2023-08-18 23:00:39 +0700

    v2

--- Array/multiLineString.js
+++ Array/multiLineString.js
@@ -1,6 +1,13 @@
 // using the + operator
-const message = 'This is a long message\n' + 
+const message1 = 'This is a long message\n' + 
     'that spans across multiple lines\n' + 
     'in the code.'
 
-console.log(message);
+console.log(message1);
+
+// using the \ operator
+const message2 = 'This is a long message\n \
+that spans across multiple lines\n \
+in the code.'
+
+console.log(message2);
