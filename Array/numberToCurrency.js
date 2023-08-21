9ba4f89b2cb356f048ce8550d9efa7fec5323c61
Yayan <111275294+hypryyn@users.noreply.github.com>
2023-08-21 22:31:30 +0700

    using concatenation

--- Array/numberToCurrency.js
+++ Array/numberToCurrency.js
@@ -4,3 +4,11 @@ const formatter = new Intl.NumberFormat('en-US', {
 });
 
 formatter.format(2500);
+
+// v2
+
+const number = 1234.5678;
+
+const result = '$ ' + number.toFixed(2);
+
+console.log(result);
