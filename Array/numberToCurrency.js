2b8a7520ab7fafa55e26584993842ddbe3b8090f
Yayan <111275294+hypryyn@users.noreply.github.com>
2023-08-22 22:58:04 +0700

    v3

--- Array/numberToCurrency.js
+++ Array/numberToCurrency.js
@@ -7,8 +7,17 @@ formatter.format(2500);
 
 // v2
 
-const number = 1234.5678;
+const number2 = 1234.5678;
 
-const result = '$ ' + number.toFixed(2);
+const result2 = '$ ' + number.toFixed(2);
 
-console.log(result);
+console.log(result2);
+
+// v3
+
+const result3 =  (2500).toLocaleString('en-US', {
+    style: 'currency',
+    currency: 'USD'
+});
+
+console.log(result3);
