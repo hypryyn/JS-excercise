0aa5d84fa60c62412986070a585b86895588c26e
Yayan <111275294+hypryyn@users.noreply.github.com>
2023-08-28 21:43:30 +0700

    v2

--- Array/startWith.js
+++ Array/startWith.js
@@ -8,3 +8,17 @@ if(string.startsWith(toCheckString)) {
 else {
     console.warn(`The string does not starts with "he".`);
 }
+
+// v2
+
+const string2 = 'hello world';
+
+const toCheckString2 = 'he';
+
+let result = string2.lastIndexOf(toCheckString, 0) === 0;
+if(result) {
+    console.warn('The string starts with "he".');
+}
+else {
+    console.warn(`The string does not starts with "he".`);
+}
