a7d0d9bb457173c5f06cd3de0331093e7a113c35
Yayan <111275294+hypryyn@users.noreply.github.com>
2023-09-02 12:28:44 +0700

    unit test

--- Array/replaceArray.js
+++ Array/replaceArray.js
@@ -4,7 +4,7 @@ function removeItemFromArray1(array1, n1) {
     const newArray = [];
 
     for ( let i = 0; i < array1.length; i++) {
-        if(array[i] !== n) {
+        if(array1[i] !== n1) {
             newArray.push(array1[i]);
         }
     }
@@ -18,18 +18,18 @@ console.log(result1);
 // v2
 
 function removeItemFromArray2(array2, n2) {
-    const index = array.indexOf(n);
+    const index = array2.indexOf(n2);
 
     // if the element is in the array, remove it
     if(index > -1) {
 
         // remove item
-        array.splice(index, 1);
+        array2.splice(index, 1);
     }
     return array2;
 }
 
-const result2 = removeItemFromArray2([1, 2, 3 , 4, 5], 2);
+const result2 = removeItemFromArray2([1, 2, 3 , 4, 5], 5);
 
 console.log(result2);
 
