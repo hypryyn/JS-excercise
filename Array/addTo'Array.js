bcef5a3723032812376ef96b854220dd2dec3133
Yayan <111275294+hypryyn@users.noreply.github.com>
2023-09-05 19:14:26 +0700

    v2

--- Array/addTo'Array.js
+++ Array/addTo'Array.js
@@ -9,3 +9,15 @@ const array1 = [1, 2, 3];
 
 addElement1(array1);
 
+// v2
+
+function addElement2(arr2) {
+
+    arr2.splice(0, 0, 4);
+    
+    console.log(arr2);
+}
+
+const array2 = [1, 2, 3];
+
+addElement2(array2);
