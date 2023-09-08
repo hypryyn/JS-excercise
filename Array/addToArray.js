8a97c3ad4205b9b2e2a8edb80a124e02bf80a725
Yayan <111275294+hypryyn@users.noreply.github.com>
2023-09-08 10:30:01 +0700

    trying overoloading concept

---
+++ Array/addToArray.js
@@ -0,0 +1,23 @@
+function addElement1(arr1) {
+
+    arr1.unshift(0, 9);
+    
+    console.log(arr1);
+}
+
+const array1 = [1, 2, 3];
+
+addElement1(array1);
+
+// v2
+
+function addElement2(arr2) {
+
+    arr2.splice(0, 0, 0, 4);
+    
+    console.log(arr2);
+}
+
+const array2 = [1, 2, 3];
+
+addElement2(array2);
---
+++ Array/overloading.js
@@ -0,0 +1,30 @@
+// program to perform function overloading
+
+function sum1() {  
+    
+    // if no argument        
+    if (arguments.length == 0) {  
+        console.log('You have not passed any argument');  
+    }
+
+    // if only one argument 
+    else if (arguments.length == 1) {  
+        console.log('Pass at least two arguments');  
+    }
+
+    // multiple arguments
+    else {
+        let result = 0;
+        let length = arguments.length;
+    
+        for (i = 0; i < length; i++) {  
+            result = result + arguments[i];  
+        }  
+        console.log(result); 
+    }  
+}
+
+sum1();
+sum1(5); 
+sum1(5, 9);    
+sum1(1, 2, 3, 4, 5, 6, 7, 8, 9); 
