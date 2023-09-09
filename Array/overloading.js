e332b43f2d545389cb1a40b28781ef2c71bcc048
Yayan <111275294+hypryyn@users.noreply.github.com>
2023-09-09 10:31:26 +0700

    trying overoloading concep v2

--- Array/overloading.js
+++ Array/overloading.js
@@ -1,4 +1,4 @@
-// program to perform function overloading
+// v1
 
 function sum1() {  
     
@@ -28,3 +28,31 @@ sum1();
 sum1(5); 
 sum1(5, 9);    
 sum1(1, 2, 3, 4, 5, 6, 7, 8, 9); 
+
+
+// v2
+
+function sum2() {
+    switch (arguments.length) {
+    case 0:
+        console.log('You have not passed any argument');
+        break;
+    case 1:
+        console.log('Pass at least two arguments');
+        break;
+    default:
+        let result = 0;
+        let length = arguments.length;
+    
+        for (i = 0; i < length; i++) {  
+            result = result + arguments[i];  
+        }  
+        console.log(result);
+        break;
+    }
+}
+
+sum2();
+sum2(5); 
+sum2(5, 9);    
+sum2(1, 2, 3, 4, 5, 6, 7, 8, 9);
