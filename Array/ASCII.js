845fdc48ea68134411f0d59fb5b898fece5bedd5
Yayan <111275294+hypryyn@users.noreply.github.com>
2023-09-11 10:58:30 +0700

    check the value in ASCII

---
+++ Array/ASCII.js
@@ -0,0 +1,9 @@
+// v1
+
+// take input from the user
+const string1 = prompt('Enter a character: ');
+
+// convert into ASCII value
+const result = string1.charCodeAt(0);
+
+console.log(`The ASCII value is: ${result}`);
